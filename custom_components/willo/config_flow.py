33a3c11076a8760063d469be7fe37ee4fb7ab39d
Daniel Moindrot <86261703+38decibel@users.noreply.github.com>
2026-04-29 10:58:12 +0200

    Add schedule entity handling in WILLO config flow

--- custom_components/willo/config_flow.py
+++ custom_components/willo/config_flow.py
@@ -2,7 +2,8 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_ADDRESS, CONF_NAME
 
-DOMAIN = "willo"
+from .const import CONF_SCHEDULE_ENTITY, DOMAIN
+
 
 class WILLOConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -11,9 +12,13 @@ class WILLOConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             await self.async_set_unique_id(user_input[CONF_ADDRESS].upper())
             self._abort_if_unique_id_configured()
+            # Normalise the optional schedule entity ID: store None if left blank
+            data = dict(user_input)
+            if not data.get(CONF_SCHEDULE_ENTITY):
+                data.pop(CONF_SCHEDULE_ENTITY, None)
             return self.async_create_entry(
                 title=user_input.get(CONF_NAME, "WILLO"),
-                data=user_input,
+                data=data,
             )
 
         return self.async_show_form(
@@ -21,5 +26,6 @@ class WILLOConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema({
                 vol.Required(CONF_ADDRESS, default="B0:B2:1C:84:61:D6"): str,
                 vol.Optional(CONF_NAME, default="WILLO"): str,
+                vol.Optional(CONF_SCHEDULE_ENTITY, default=""): str,
             }),
         )
