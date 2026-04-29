2be842c416990b4ca5f2eaca7e394521c1891cd4
Daniel Moindrot <86261703+38decibel@users.noreply.github.com>
2026-04-29 10:59:37 +0200

    Add constants for time slot scheduling system

--- custom_components/willo/const.py
+++ custom_components/willo/const.py
@@ -1,3 +1,9 @@
-"""Constantes pour l'intégration WILLO."""
+"""Constants for the WILLO integration."""
 
 DOMAIN = "willo"
+
+CONF_SCHEDULE_ENTITY = "schedule_entity_id"
+CONF_SLOT1_START = "slot1_start"
+CONF_SLOT1_END = "slot1_end"
+CONF_SLOT2_START = "slot2_start"
+CONF_SLOT2_END = "slot2_end"
